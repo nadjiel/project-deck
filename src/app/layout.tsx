c82b192a48efdf13781a6e9b4e4d2d921bf1ee0e
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-02 22:28:00 -0300

    :art: style: respaced root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Header />
-        {children}
+        <main className="flex-1">{children}</main>
         <Navbar />
       </body>
     </html>
