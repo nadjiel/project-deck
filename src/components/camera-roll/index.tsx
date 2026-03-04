85b356837dcac87d305d3b6e7b4d896b26e887b5
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-03 22:14:54 -0300

    :art: style: added animation to camera roll

--- src/components/camera-roll/index.tsx
+++ src/components/camera-roll/index.tsx
@@ -2,6 +2,13 @@ import profile from "@/assets/profile.jpg";
 
 export default function CameraRoll() {
   return (
-    <img src={profile.src} className="size-64 object-cover rounded-full" />
+    <div className="relative size-64">
+      <img src={profile.src} className="size-full object-cover rounded-full" />
+      <div className="absolute top-0 size-full animate-spin animation-duration-20000">
+        <div className="absolute top-3/20 left-2/6 -translate-1/2 size-8 rounded-full bg-background"></div>
+        <div className="absolute top-1/8 left-1/2 -translate-1/2 size-10 rounded-full bg-background"></div>
+        <div className="absolute top-3/20 left-4/6 -translate-1/2 size-8 rounded-full bg-background"></div>
+      </div>
+    </div>
   );
 }
