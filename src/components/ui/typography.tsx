186858ecabaa7d8f53aec07d316806630340592d
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-04 21:17:30 -0300

    :sparkles: feat: typography component

---
+++ src/components/ui/typography.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const HEADING_LEVELS = [1, 2, 3, 4, 5, 6] as const;
+
+const HEADING_TYPES = ["h1", "h2", "h3", "h4", "h5", "h6"] as const;
+
+type HeadingLevel = typeof HEADING_LEVELS[number];
+
+type HeadingType = typeof HEADING_TYPES[number];
+
+interface HeadingProps extends
+  React.ComponentProps<HeadingType>,
+  VariantProps<typeof headingVariants>
+{
+  level?: HeadingLevel;
+};
+
+const headingVariants = cva(
+  "",
+  {
+    variants: {
+      variant: {
+        h1: "font-medium text-4xl",
+        h2: "font-medium text-2xl",
+        h3: "font-medium text-2xl",
+        h4: "font-medium text-2xl",
+        h5: "font-medium text-2xl",
+        h6: "font-medium text-2xl",
+      },
+    },
+    defaultVariants: {
+      variant: "h1",
+    },
+  }
+)
+
+function Heading(props: HeadingProps) {
+  const {
+    level = 1,
+    variant = "h" + level as HeadingType,
+    children,
+    className,
+    ...rest
+  } = props;
+
+  const Comp = "h" + level as HeadingType;
+
+  return (
+    <Comp
+      className={cn(headingVariants({ variant, className }))}
+      {...rest}
+    >
+      {children}
+    </Comp>
+  )
+}
+
+export { Heading };
