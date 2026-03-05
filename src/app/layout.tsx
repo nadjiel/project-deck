a42b5c2cba46ac57de7cb39be6bcfd0a53d66e9b
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-04 21:35:43 -0300

    :art: style: tweaked layout spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex flex-col flex-1 px-4">{children}</main>
         <Navbar />
       </body>
     </html>
