4b378d5e9be671af7a9d752e0a72459c7a0a45e4
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-06 21:44:12 -0300

    :art: style: added tactile variant to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        tactile: "bg-primary border-primary text-primary-foreground border-b-4 transition-none hover:bg-primary/90 hover:border-primary/90 active:border-b-0",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
