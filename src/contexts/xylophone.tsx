71533c39b4733a6881f9236d2af53cd50e4cf7a0
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-07 21:27:49 -0300

    :sparkles: feat: xylophone context

---
+++ src/contexts/xylophone.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+import chroma from "chroma-js";
+import { getVariable } from "@/lib/css";
+import { play } from "@/lib/xylophone";
+
+interface XylophoneBar {
+  color: string;
+  note: string;
+  play: () => void;
+}
+
+interface XylophoneValue {
+  gradient: string[];
+  baseNote: string;
+  bars: number;
+}
+
+interface Props extends PropsWithChildren {
+  gradient?: string[];
+  baseNote?: string;
+  bars: number;
+}
+
+const defaultValue: XylophoneValue = {
+  gradient: [],
+  baseNote: "C4",
+  bars: 1,
+};
+
+const Context = createContext<XylophoneValue>(defaultValue);
+
+export default function Xylophone(props: Props) {
+  const {
+    gradient: propGradient,
+    baseNote = "C4",
+    bars,
+    children
+  } = props;
+
+  const [gradient, setGradient] = useState(propGradient ?? []);
+
+  useEffect(() => {
+    if (propGradient) return;
+
+    setGradient([
+      chroma(getVariable("background")).brighten().hex(),
+      chroma(getVariable("primary")).hex(),
+    ]);
+  }, []);
+
+  return (
+    <Context.Provider value={{
+      gradient,
+      baseNote,
+      bars,
+    }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export function useXylophone(bar: number): XylophoneBar {
+  const { gradient, bars } = useContext(Context);
+
+  const defaultNote = "C4";
+
+  return {
+    color: chroma.scale(gradient).colors(bars)[bar],
+    note: defaultNote,
+    play: (note = defaultNote) => play(note)
+  };
+}
