a711dbbf33e9a59c76fc3332642f2fefec8c14bf
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-08 00:05:37 -0300

    :sparkles: feat: added abilities to their page

--- src/app/abilities/page.tsx
+++ src/app/abilities/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/ui/typography";
+import { Xylophone } from "@/feat/xylophone";
+import api, { type Ability } from "@/api";
+import AbilityButton from "@/components/ability-button";
 
 export default function Abilities() {
+  const [abilities, setAbilities] = useState<Ability[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await api.fetch();
+
+      setAbilities(data.abilities);
+    };
+
+    fetch();
+  }, []);
+
   return (
     <div className="flex flex-col flex-1 gap-4 items-center">
       <Heading variant="h1">What can I do for you?</Heading>
@@ -10,6 +28,11 @@ export default function Abilities() {
         <li>Develop scalable fullstack applications</li>
         <li>Provide translation services</li>
       </ul>
+      <Xylophone bars={abilities.length}>
+        <div className="flex gap-2">
+          { abilities.map(a => <AbilityButton key={a.id} data={a} />) }
+        </div>
+      </Xylophone>
     </div>
   );
 }
