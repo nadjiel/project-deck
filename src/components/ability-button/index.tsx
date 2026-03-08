fb9e9b8fb8dfaa20b6469851255676f29097a8a8
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-08 00:03:47 -0300

    :sparkles: feat: dedicated ability button

---
+++ src/components/ability-button/index.tsx
@@ -0,0 +1,17 @@
+import * as icons from "react-icons/si";
+import { XylophoneButton } from "@/feat/xylophone";
+import type { Ability } from "@/api";
+
+interface Props {
+  data: Ability;
+}
+
+export default function AbilityButton(props: Props) {
+  const { data } = props;
+
+  const Icon = icons[data.icon as keyof typeof icons];
+
+  return (
+    <XylophoneButton bar={data.id - 1}><Icon /></XylophoneButton>
+  )
+}
