9d0c1413a22425499a03b60681f8629cbbda5e54
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-08 14:55:40 -0300

    :sparkles: feat: created search-box

---
+++ src/components/search-box/index.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+import { useSearch } from "@/hooks/use-search";
+
+export default function SearchBox() {
+  const { internalSearch, setSearch } = useSearch({ debounce: 1000 });
+
+  return (
+    <Input
+      value={internalSearch}
+      onChange={e => setSearch(e.target.value)}
+    />
+  );
+}
