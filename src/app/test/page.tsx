7709dae1b0536a3c13d0b2a5699c5eac6297b2e0
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-13 23:16:30 -0300

    :sparkles: feat: setup test project dialog

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,49 @@
-"use client";
+import { cookies } from "next/headers";
+import {
+  Dialog,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { CardDialogContent } from "@/components/card-dialog-content";
+import { createClient } from "@/db/supabase/server";
+import { ProjectDialog } from "@/feat/project";
 
-import Swiper from "@/components/swiper";
+export default async function Test() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
 
-export default function Test() {
+  const { data: project } = await supabase
+    .from("projects")
+    .select(`
+      *,
+      abilities:project_abilities (
+        level,
+        ability:abilities (
+          name,
+          icon
+        )
+      )
+    `)
+    .eq("name", "Im Share")
+    .single();
+
+  if (project === null) throw new Error("Impossible to load project");
+
   return (
-    <Swiper />
+    <Dialog >
+      <DialogTrigger>Open</DialogTrigger>
+      <CardDialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <ProjectDialog data={project} />
+      </CardDialogContent>
+    </Dialog>
   );
 }
