0ca4113c9a2c9c29080808487974b9d9a961cc51
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-21 18:21:19 -0300

    :sparkles: feat: structured contact form

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -1,11 +1,57 @@
-import { Input } from "@/components/ui/input";
+"use client";
 
-export default function ContactForm() {
+import { useForm } from "react-hook-form";
+import { email } from "@/config/env";
+import { Input } from "@/components/ui/input";
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+interface ContactData {
+  name: string;
+  email: string;
+}
+
+interface Props {
+  placeholders?: ContactData;
+}
+
+export default function ContactForm(props: Props) {
+  const { placeholders } = props;
+
+  const { register } = useForm();
+
   return (
-    <form action="https://formsubmit.co/your@email.com" method="POST">
-      <Input type="text" name="name" required />
-      <Input type="email" name="email" required />
-      <button type="submit">Send</button>
+    <form action={`https://formsubmit.co/${email}`} method="POST">
+      <Field>
+        <FieldLabel htmlFor="name">Name</FieldLabel>
+        <Input
+          id="name"
+          placeholder={placeholders?.name || "Your name..."}
+          {...register("name")}
+        />
+        <FieldError>Error</FieldError>
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="email">Email</FieldLabel>
+        <Input
+          id="email"
+          placeholder={placeholders?.email || "Your email address..."}
+          {...register("email")}
+        />
+        <FieldError>Error</FieldError>
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="message">Message</FieldLabel>
+        <Textarea
+          id="message"
+          placeholder={"What can I do for you?"}
+          {...register("message")}
+        />
+        <FieldError>Error</FieldError>
+      </Field>
+
+      <Button type="submit">Send</Button>
     </form>
   );
 }
