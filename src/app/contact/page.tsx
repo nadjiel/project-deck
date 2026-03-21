f1886becb2f69ee2e3f45c00b26798d3a5849131
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-03-21 18:21:43 -0300

    :sparkles: feat: supplied contact placeholders

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,18 @@
+import { faker } from "@faker-js/faker";
 import ContactForm from "@/components/contact-form";
 
 export default function Contact() {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+
+  const placeholders = {
+    name: faker.person.fullName({ firstName, lastName }),
+    email: faker.internet.email({ firstName, lastName }).toLowerCase(),
+  }
+
   return (
-    <div className="bg-red-200 flex-1">
-      <ContactForm />
+    <div className="bg-red-950 flex-1">
+      <ContactForm placeholders={placeholders} />
     </div>
   );
 }
