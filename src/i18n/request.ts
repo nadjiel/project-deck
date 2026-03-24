42eb57b78aed4cd52f0dbea2431171f86c85d62a
Daniel de Oliveira <daniel@fenixtecnologia.info>
2026-03-24 13:52:50 +0000

    :recycle: refac: locale request with hasLocale function

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,13 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
+import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale as typeof routing.locales[number] | undefined;
-
-  // Ensure a valid locale is used
-  if (!locale || !routing.locales.includes(locale)) {
-    locale = routing.defaultLocale;
-  }
+  const requested = await requestLocale;
+  const locale = hasLocale(routing.locales, requested)
+    ? requested
+    : routing.defaultLocale;
 
   return {
     locale,
