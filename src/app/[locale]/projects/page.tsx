5e8936009ab1f429f50c142b73797d42d6e3c454
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-24 13:29:20 -0300

    :sparkles: feat: translated projects page

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
+import { getTranslations } from "next-intl/server";
 import { Heading } from "@/components/ui/typography";
-import { Button } from "@/components/ui/button";
 import SearchBox from "@/components/search-box";
 import { ProjectLayout, ProjectLayoutButton } from "@/feat/project";
 import { createClient } from "@/db/supabase/server";
@@ -8,6 +8,8 @@ import { createClient } from "@/db/supabase/server";
 export default async function Projects() {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
+
+  const t = await getTranslations("projects");
 
   const { data: projects } = await supabase
     .from("projects")
@@ -32,9 +34,9 @@ export default async function Projects() {
   return (
     <div className="flex flex-col flex-1 items-center">
       <div className="flex flex-col items-center gap-2 w-full">
-        <Heading variant="h1">Know my Work!</Heading>
+        <Heading variant="h1">{t("title")}</Heading>
         <div className="flex gap-2">
-          <SearchBox placeholder="Search a project..." className="max-w-sm" />
+          <SearchBox placeholder={t("search")} className="max-w-sm" />
           <ProjectLayoutButton />
         </div>
       </div>
