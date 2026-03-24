4d68afd57f1a2f87f087ba918405cc8c88fe9688
Daniel de Oliveira <daniel@fenixtecnologia.info>
2026-03-24 13:44:13 +0000

    :sparkles: feat: intl provider on root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,9 @@
+import { NextIntlClientProvider } from "next-intl";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
+import type { PropsWithChildren } from "react";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
@@ -9,18 +11,16 @@ export const metadata: Metadata = {
   description: "Nayel's portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex flex-col flex-1 px-4 pt-8 pb-32">{children}</main>
-        <Navbar />
-        <Toaster position="top-center" />
+        <NextIntlClientProvider>
+          <Header />
+          <main className="flex flex-col flex-1 px-4 pt-8 pb-32">{children}</main>
+          <Navbar />
+          <Toaster position="top-center" />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
