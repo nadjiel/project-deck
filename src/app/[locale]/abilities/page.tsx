c3066dca29802dab142253945a4ec39da4f5c89e
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-24 13:23:30 -0300

    :sparkles: feat: translated abilities page

--- src/app/[locale]/abilities/page.tsx
+++ src/app/[locale]/abilities/page.tsx
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import { getTranslations } from "next-intl/server";
 import { Heading } from "@/components/ui/typography";
 import { Xylophone } from "@/feat/xylophone";
 import AbilityButton from "@/components/ability-button";
@@ -8,6 +9,8 @@ export default async function Abilities() {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
+  const t = await getTranslations("abilities");
+
   const { data: abilities } = await supabase.from("abilities").select();
 
   if (abilities === null) throw new Error("Impossible to load abilities");
@@ -15,12 +18,12 @@ export default async function Abilities() {
   return (
     <div className="flex flex-col flex-1 justify-between items-center">
       <div className="flex flex-col gap-4 items-center">
-        <Heading variant="h1">What can I do for you?</Heading>
+        <Heading variant="h1">{t("title")}</Heading>
         <ul className="list-disc">
-          <li>Craft distinctive memorable websites</li>
-          <li>Conceive thoughtful prototypes and wireframes</li>
-          <li>Develop scalable fullstack applications</li>
-          <li>Provide translation services</li>
+          <li>{t("service1")}</li>
+          <li>{t("service2")}</li>
+          <li>{t("service3")}</li>
+          <li>{t("service4")}</li>
         </ul>
       </div>
       <Xylophone bars={abilities.length}>
