e2dc1049307c6e16957ab37637cd08d48ed364ff
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-03-29 10:56:01 -0300

    :art: style: added hover info to related projects

--- src/feat/project/components/project-link.tsx
+++ src/feat/project/components/project-link.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ArrowUpRightIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { HoverCard, HoverCardTrigger, HoverCardContent } from "@/components/ui/hover-card";
 import type { Project } from "@/feat/project/types";
 
 interface Props {
@@ -11,19 +12,26 @@ export default function ProjectLink(props: Props) {
   const { data } = props;
 
   return (
-    <Link
-      key={data.id}
-      href={`/projects/${data.slug}`}
-      target="_blank"
-      className={cn(
-        "flex items-center leading-tight",
-        "pr-4 [&>div]:w-0 transition-all hover:underline hover:pr-0 hover:[&>div]:w-4",
-      )}
-    >
-      <span>{data.name}</span>
-      <div className="overflow-hidden transition-all">
-        <ArrowUpRightIcon size="16" />
-      </div>
-    </Link>
+    <HoverCard>
+      <HoverCardTrigger>
+        <Link
+          key={data.id}
+          href={`/projects/${data.slug}`}
+          target="_blank"
+          className={cn(
+            "flex items-center leading-tight",
+            "pr-4 [&>div]:w-0 transition-all hover:underline hover:pr-0 hover:[&>div]:w-4",
+          )}
+        >
+          <span>{data.name}</span>
+          <div className="overflow-hidden transition-all">
+            <ArrowUpRightIcon size="16" />
+          </div>
+        </Link>
+      </HoverCardTrigger>
+      <HoverCardContent>
+        <p>{data.description}</p>
+      </HoverCardContent>
+    </HoverCard>
   )
 }
