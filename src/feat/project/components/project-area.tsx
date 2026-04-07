6beba0c5dd234f1428001093d3afa7fc2f2f74e0
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-04-07 13:34:17 -0300

    :art: style: disabled project area dragging for scroll

--- src/feat/project/components/project-area.tsx
+++ src/feat/project/components/project-area.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 import { motion } from "motion/react";
 import { ProjectCard } from "@/feat/project";
 import { cn } from "@/lib/utils";
@@ -21,7 +21,7 @@ export default function ProjectArea(props: Props) {
 
   const { filters } = useFilters(["abilities"]);
   
-  const dragRef = useRef<HTMLDivElement>(null);
+  // const dragRef = useRef<HTMLDivElement>(null);
 
   const filtered = projects.filter(
     p => p.abilities.some(
@@ -31,9 +31,10 @@ export default function ProjectArea(props: Props) {
 
   return (
     <div
-      ref={dragRef}
+      // ref={dragRef}
       className={cn(
-        "relative flex flex-col flex-1 overflow-hidden w-dvw",
+        "relative flex flex-col flex-1 overflow-x-auto w-dvw",
+        "[&::-webkit-scrollbar]:h-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-accent [&::-webkit-scrollbar-thumb]:bg-primary",
         "before:absolute before:left-0 before:w-64 before:h-full before:bg-linear-to-r before:from-background before:to-transparent before:z-1 before:pointer-events-none",
         "after:absolute after:right-0 after:w-64 after:h-full after:bg-linear-to-l after:from-background after:to-transparent after:z-1 after:pointer-events-none",
         className,
@@ -41,10 +42,10 @@ export default function ProjectArea(props: Props) {
       {...rest}
     >
       <motion.div
-        drag="x"
-        dragConstraints={dragRef}
+        // drag="x"
+        // dragConstraints={dragRef}
         // whileDrag={{ pointerEvents: "none" }}
-        className="flex flex-1 items-center gap-4 w-max px-8 touch-none"
+        className="flex flex-1 items-center gap-4 w-max px-8"
       >
         { filtered.map(p => (
           <ProjectCard
