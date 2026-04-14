21fa19ac15c95599edb36bded83d4aceee96206e
Daniel de Oliveira <97067805+nadjiel@users.noreply.github.com>
2026-04-13 22:58:55 -0300

    :bug: fix: connection from local dev origin

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.*"],
   images: {
     remotePatterns: [
       {
